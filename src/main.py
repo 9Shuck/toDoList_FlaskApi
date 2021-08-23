1c2aad1af21641304647f63a247505e01cd5c980
9Shuck <rubb.phz@gmail.com>
2021-08-23 15:07:30 +0000

    final

--- src/main.py
+++ src/main.py
@@ -1,6 +1,3 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,9 +5,11 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+#Import Each Table
+from models import db, Account, Task
+import json
 
+#app (If Blueprint set in url /api)
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
@@ -20,24 +19,111 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
+
+#Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
+#Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#Declaración de métodos pedidos por Jimena
+#1-Ver todas las cuentas
+#GetAccount method
+@app.route('/account', methods=['GET'])
+def get_user():
+    all_user = Account.get_all() 
+    if all_user:
+        return jsonify(all_user), 200
 
-    return jsonify(response_body), 200
+    return jsonify({'message': 'No account created'}), 500
+
+#2-Crear una cuenta
+#PostAccount method (new Account)
+@app.route('/account', methods=['POST'])
+def create_user_task():
+
+    nick = request.json.get('nick', None)
+    
+    if not (nick):
+        return {'error': 'Missing info'}, 400
+
+    user= Account(nick = nick)
+    user.create()
+
+    return jsonify(user.to_dict()),201
+
+#3-Borrar una cuenta por id
+#DeleteAccount method 
+@app.route('/account/<int:id>', methods = ['DELETE'])
+def delete_account(id):
+    account = Account.get_by_id(id)
+
+    if account:
+        account.delete()
+        return jsonify(account.to_dict()), 200
+
+    return jsonify({'msg' : 'Account not foud'}), 404
+
+#4-Ver todas las tareas de un usuario 
+#Get all tasks from user ID
+@app.route('/account/<int:id>/tasks', methods=['GET'])
+def get_task(id):
+
+    user_tasks = Task.get_task_by_user(id) 
+
+    if not user_tasks:
+        return jsonify({'message': 'No tasks yet'}), 200
+
+    return jsonify(user_tasks), 200
+    
+#5-Ver una tarea en concreto
+@app.route('/account/<int:id>/tasks/<int:position>', methods=['GET'])
+def get_specific_task(id, position):
+
+    #add filtrado por status active y por position.
+
+    user_tasks = Task.get_one_task(position) 
+
+    if not user_tasks:
+        return jsonify({'message': 'No tasks yet'}), 200
+
+    return jsonify(user_tasks), 200
+
+#6-Añadir una tarea
+@app.route('/account/<int:id>/tasks', methods=['POST'])
+def add_new_task(id):
+    label = request.json.get('label', None)
+    
+    
+    if not (label and id):
+        return {'error': 'No enough info'}, 400
+
+    task= Task(label = label, account_id=id)
+    task.add_new()
+
+    return jsonify(task.to_dict()),201
+
+    print("Incoming request", request_body)
+    return jsonify(task)
+
+
+#7-Borrar una tarea
+@app.route('/account/<int:id>/tasks/<int:position>', methods=['DELETE'])
+def delete_user_task(id, position):
+    task_to_delete = Task.get_one_task(position)
+    if task_to_delete:
+        if task_to_delete.account_id == id:
+            task_to_delete.delete()
+            return jsonify(task_to_delete.to_dict()), 200
+
+    return {'error': 'Access denied'}, 400
+    
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,110 @@
+import os
+import sys
+from sqlalchemy import Table, Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import exc
+import json
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Account(db.Model):
+    __tablename__ = 'account'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    nick = db.Column(db.String(250), unique=True,nullable=False)
+    assign_tasks = db.relationship('Task', lazy=True)
+
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'Account: {self.id}, user: {self.nick}'
 
-    def serialize(self):
+
+    def to_dict(self):
+        print(self.assign_tasks)
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "nick": self.nick,
         }
+
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_all(cls):
+        users_list = cls.query.all()
+        return [user.to_dict() for user in users_list]
+
+
+    @classmethod
+    def get_by_id(cls,id):
+        account = cls.query.filter_by(id = id).one_or_none()
+        return account
+
+    def update(slef, nick):
+        self.nick = nick
+        db.session.commit()
+
+    
+    @classmethod
+    def get_by_nick(cls,nick):
+        account = cls.query.filter_by(nick = nick).one_or_none()
+        return account 
+    
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Task(db.Model): 
+    __tablename__ = 'task'
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column (db.String(250),nullable=False)
+    status = db.Column (db.Boolean(True), default=False )
+    account_id = db.Column (db.Integer, db.ForeignKey("account.id"))
+
+
+    def __repr__(self):
+        return f'Task: {self.id}, label: {self.label}, status: {self.status} from user: {self.account_id}'
+    
+
+    def to_dict(self):
+        account = Account.get_by_id(self.account_id)
+        return{
+            "task_id":self.id,
+            "label":self.label,
+            "status":self.status,
+            "account": account.nick
+        }
+
+
+    def add_new(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    @classmethod
+    def get_tasks(cls):
+        tasks = cls.query.all()
+        return [task.to_dict() for task in tasks]
+
+
+    @classmethod
+    def get_task_by_user(cls, id):
+        specific_task_list = cls.query.filter_by(account_id = id, status =False)
+        return [element.to_dict() for element in specific_task_list]
+
+
+    @classmethod
+    def get_one_task(cls, position):
+        one_task = cls.query.get(position)
+        return one_task
+
+#NO DEVOLVER one_task.dict; eso lo devolverá en main
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
